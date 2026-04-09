268256ab823d6439561b3a574075144aa9ba0cd5
Ranilo <amandamenezes594@gmail.com>
2026-04-09 09:21:36 -0300

    feat: creating initial structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+// pagina-de-login/src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
